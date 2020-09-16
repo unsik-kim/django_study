f1f355e5803b810aa13b68e406c1658b9323889e
unsik-kim <loudic92@gmail.com>
2020-09-16 17:39:03 +0900

    commit

--- exprj1/helloprj/hello2/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Hello2Config(AppConfig):
-    name = 'hello2'
--- exprj1/helloprj/helloprj/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('123', include('hello.urls')),
-    path('hello2/', include('hello2.urls')),
-    path('admin/', admin.site.urls),
-]
